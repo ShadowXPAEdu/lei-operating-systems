d9a6af2704e8c33b71a0e043eb20e6dc96f0e019
ShadowXPA <51165769+ShadowXPA@users.noreply.github.com>
2019-10-26 20:51:44 +0100

    Add files via upload

---
+++ msgdist_client.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "msgdist.h"
+
+int main(int argc, char* argv[], char** envp) {
+    if (argc != 2) {
+        printf("You need to specify a username!\nTry %s [username]\n", argv[0]);
+        return -1;
+    } else {
+
+    }
+    return 0;
+}
---
+++ msgdist_server.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "msgdist.h"
+
+int main(int argc, char* argv[], char** envp) {
+
+
+
+    return 0;
+}
