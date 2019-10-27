2bce33b20cf2b98f5d956b068b4e48b36da47d2d
ShadowXPA <51165769+ShadowXPA@users.noreply.github.com>
2019-10-27 15:55:05 +0000

    Add files via upload

--- msgdist_server.c
+++ msgdist_server.c
@@ -30,28 +30,62 @@ int main(int argc, char* argv[], char** envp) {
             char adm_cmd[32];
             printf("Admin > ");
             fgets(adm_cmd, 32, stdin);
-            if (strcmp(adm_cmd, "filter on\n") == 0) {
-                // turn on filter
-            } else if (strcmp(adm_cmd, "filter off\n") == 0) {
-                // turn off filter
-            } else if (strcmp(adm_cmd, "users\n") == 0) {
-                // list users
-            } else if (strcmp(adm_cmd, "topics\n") == 0) {
-                // list topics
-            } else if (strcmp(adm_cmd, "msg\n") == 0) {
-                // list messages
-            } else if (strcmp(adm_cmd, "topic ...\n") == 0) {
-                // list topic x
-            } else if (strcmp(adm_cmd, "del ...\n") == 0) {
-                // del message x
-            } else if (strcmp(adm_cmd, "kick ...\n") == 0) {
-                // kick user x
-            } else if (strcmp(adm_cmd, "prune\n") == 0) {
-                // delete topics with no messages and cancel subscriptions
-            } else if (strcmp(adm_cmd, "shutdown\n") == 0) {
-                i = 1;
-            } else {
-                printf("Unknown or incomplete command...\n");
+            char *ptr = strtok(adm_cmd, "\n ");
+            if (ptr != NULL) {
+                ptr = strtok(NULL, "\n ");
+                if (ptr == NULL) {
+                    if (strcmp(adm_cmd, "filter") == 0) {
+                        // filter help
+                        printf("Incomplete command.\nTo use '%s', try '%s [on/off]'\n", adm_cmd, adm_cmd);
+                    } else if (strcmp(adm_cmd, "users") == 0) {
+                        // list users
+                    } else if (strcmp(adm_cmd, "topics") == 0) {
+                        // list topics
+                    } else if (strcmp(adm_cmd, "msg") == 0) {
+                        // list messages
+                    } else if (strcmp(adm_cmd, "topic") == 0) {
+                        // topic help
+                        printf("Incomplete command.\nTo use '%s', try '%s [topic_name]'\n", adm_cmd, adm_cmd);
+                    } else if (strcmp(adm_cmd, "del") == 0) {
+                        // del help
+                        printf("Incomplete command.\nTo use '%s', try '%s [message_id]'\n", adm_cmd, adm_cmd);
+                    } else if (strcmp(adm_cmd, "kick") == 0) {
+                        // kick help
+                        printf("Incomplete command.\nTo use '%s', try '%s [user_id]'\n", adm_cmd, adm_cmd);
+                    } else if (strcmp(adm_cmd, "prune") == 0) {
+                        // delete topics with no messages and cancel subscriptions
+                    } else if (strcmp(adm_cmd, "shutdown") == 0) {
+                        i = 1;
+                    } else if (strcmp(adm_cmd, "help") == 0) {
+                        printf("Here's a list of all commands currently available:\n");
+                        printf("filter [on/off] - Turns message filtering on or off.\n");
+                        printf("users - Lists all users currently connected to the server.\n");
+                        printf("topics - Lists all topics currently stored on the server.\n");
+                        printf("msg - Lists all messages currently stored on the server.\n");
+                        printf("topic [topic_name] - Lists all messages with the topic_name.\n");
+                        printf("del [message_id] - Deletes the message associated with the message_id.\n");
+                        printf("kick [user_id] - Kicks the user associated with the user_id.\n");
+                        printf("prune - Deletes topics with no messages associated and cancel subscriptions to those topics.\n");
+                        printf("shutdown - Shuts the server down.\n");
+                        printf("help - Shows this.\n");
+                    } else {
+                        printf("Unknown command...\n");
+                    }
+                } else {
+                    if (strcmp(adm_cmd, "filter") == 0 && strcmp(ptr, "on") == 0) {
+                        // turn filter on
+                    } else if (strcmp(adm_cmd, "filter") == 0 && strcmp(ptr, "off") == 0) {
+                        // turn filter off
+                    } else if (strcmp(adm_cmd, "topic") == 0) {
+                        // show messages with topic [ptr]
+                    } else if (strcmp(adm_cmd, "del") == 0) {
+                        // delete message with msg_id [ptr]
+                    } else if (strcmp(adm_cmd, "kick") == 0) {
+                        // kick user with user_id [ptr]
+                    } else {
+                        printf("Unknown command...\n");
+                    }
+                }
             }
         }
 
