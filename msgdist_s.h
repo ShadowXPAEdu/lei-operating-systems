b653f373aff5b43d90bc2552a6f23062c7f29925
ShadowXPA <51165769+ShadowXPA@users.noreply.github.com>
2019-11-01 16:49:12 +0000

    Delete msgdist_s.h

--- msgdist_s.h
+++
@@ -1,9 +0,0 @@
-#define PERR_NORM 0
-#define PERR_ERROR 1
-#define PERR_WARNING 2
-#define PERR_INFO 3
-
-void sv_exit(int return_val);
-void *cmd_reader();
-
-void printerr(const char *str, int val);
